db2746c60cdc6044c5acfcc50f6793b2c9a865a4
Randa wehbe <randawehbe81@gmail.com>
2025-07-17 00:51:44 +0300

    Deploy Flutter web build to /app and update landing page 1
    -- INSERT --
    :WQ

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "373234d664ffe656d0fb4039a86ad513
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "0185d9ddeb430755b62d6d4a01beb838",
+"flutter_bootstrap.js": "0841c77493301398412ea2422ae961ec",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
