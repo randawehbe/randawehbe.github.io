f2b8ee56322e2e9a85c9282c5e783ca5f2cd57e0
Randa wehbe <randawehbe81@gmail.com>
2025-07-20 09:37:19 +0300

    Deploy Flutter web build

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1137337603"
+    serviceWorkerVersion: "2644868840"
   }
 });
